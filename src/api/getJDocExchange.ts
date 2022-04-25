a26ee94dce0df9f496069d2b5ec902558776d3ee
alvitsit <al.tselischev@gmail.com>
2022-04-25 14:58:37 +0400

    fix inline scripts and styles in export mode #2

--- src/api/getJDocExchange.ts
+++ src/api/getJDocExchange.ts
@@ -1,8 +1,6 @@
 import {runRequest} from 'utils/runRequest';
 import {JDocType} from './getResources.model';
 
-// const baseUrl = process.env.REACT_APP_API_URL || `${window.location.origin}/api`;
-
-const baseUrl = 'https://dev.editor.jsight.io/api';
+const baseUrl = process.env.REACT_APP_API_URL || `${window.location.origin}/api`;
 
 export const getJDocExchange = (body: string) => runRequest<JDocType>(baseUrl, {body});
