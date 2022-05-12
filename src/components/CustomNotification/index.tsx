7ad89d7e95331107e2e92bc4cd4ec2a18039fa12
alvitsit <al.tselischev@gmail.com>
2022-05-12 17:40:01 +0400

    rename CustomNotification styles

--- src/components/CustomNotification/index.tsx
+++ src/components/CustomNotification/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './CustomNotification.style.scss';
+import './CustomNotification.styles.scss';
 
 export interface CustomNotificationsProps {
   message: string | JSX.Element;
