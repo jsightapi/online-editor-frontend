bb237d5699dafcfe298de2721bb44f6fd0c5c530
alvitsit <al.tselischev@gmail.com>
2022-05-31 15:28:06 +0400

    error notification styles fixed

--- src/utils/showEditorError.ts
+++ src/utils/showEditorError.ts
@@ -4,6 +4,7 @@ import {toast} from 'react-toastify';
 import {getError, getErrorTitle} from 'utils/getError';
 import {notificationIds} from 'utils/notificationIds';
 import {EditorErrorNotification} from 'components/Notifications/EditorErrorNotification';
+import {IconError} from 'components/Notifications/IconError';
 
 const {ERROR_MESSAGE_DEFAULT_ID} = notificationIds;
 
@@ -15,19 +16,18 @@ const showErrorOptions: ToastOptions = {
   hideProgressBar: true,
   closeButton: false,
   draggable: false,
+  icon: IconError(),
 };
 
 export const showEditorError = (error: ErrorType, setScrollToRow: () => void) => {
   const title = getErrorTitle(error);
   const message = getError(error);
 
-  const render = (
-    <EditorErrorNotification setScrollToRow={setScrollToRow} title={title} message={message} />
-  );
-
   if (!toast.isActive(ERROR_MESSAGE_DEFAULT_ID)) {
-    toast.warning(render, showErrorOptions);
+    toast.warning(EditorErrorNotification({setScrollToRow, title, message}), showErrorOptions);
   } else {
-    toast.update(ERROR_MESSAGE_DEFAULT_ID, {render});
+    toast.update(ERROR_MESSAGE_DEFAULT_ID, {
+      render: EditorErrorNotification({setScrollToRow, title, message}),
+    });
   }
 };
