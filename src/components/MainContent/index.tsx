81559e4b1cff6212b97171db0820b5fe296e34ef
Alexandr <102429902+alvitsit@users.noreply.github.com>
2022-06-08 18:31:50 +0400

    scrolling docmentation when changed code fixed (#106)

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -15,6 +15,7 @@ import {ResourceState, SchemaViewType, SelectedLineType} from 'store/MainStore';
 import './MainContent.styles.scss';
 import {GlobalSettingsContext} from 'components/Layout';
 import {SchemaData} from 'components/CodeView/Code';
+import {usePrevious} from 'hooks/usePrevious';
 
 type SchemaPropertyType =
   | 'collapsedRules'
@@ -44,6 +45,8 @@ export const MainContent = React.memo(({jdocExchange, showRightSidebar}: MainCon
   const [schemasView, setSchemasView] = useState<SchemaViewType[]>([]);
   const [schemasData, setSchemasData] = useState<{[key: string]: SchemaData[]}>({});
   const [resourceState, setResourceState] = useState<ResourceState[]>([]);
+  const prevPath = usePrevious(path);
+  const prevCurrentUrl = usePrevious(currentUrl);
 
   const {isExport} = window as any;
 
@@ -236,7 +239,7 @@ export const MainContent = React.memo(({jdocExchange, showRightSidebar}: MainCon
 
     const index = jdocPositions.indexOf(`${currentPath?.replace(/({|})/gi, '-')}`);
 
-    if (~index && virtuosoRef?.current) {
+    if (~index && virtuosoRef?.current && (prevCurrentUrl !== currentUrl || prevPath !== path)) {
       virtuosoRef.current.scrollToIndex({
         index: index + 1,
         align: 'start',
