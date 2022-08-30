8a2c9757df63c7ea0734b166376c42292758fe62
Alexandr <102429902+alvitsit@users.noreply.github.com>
2022-08-30 23:42:31 +0400

    JDOC-704 (#161)
    * fixed editor opening by url without version
    
    * type option of rules in detail card
    
    * fixed display of enum values as a part of the 'or' rule
    
    * Enum values displayed as a part of the 'or' rule fixed
    
    * refactoring links and redirecting on content sidebar
    
    * fixed scrolling when http method is '/', fixed scrolling to enums

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -198,7 +198,7 @@ export const MainContent = React.memo(
                     'path'
                   ),
                   (interactionsByPath, interactionPath) => {
-                    const resourceKey = `${interactionPath.slice(1).replace(/({|})/gi, '-')}`;
+                    const resourceKey = `${interactionPath.replace(/({|})/gi, '-')}`;
                     jdocList.push(
                       <HttpResource
                         resourceKey={`${tagKey}-${resourceKey}`}
@@ -217,14 +217,13 @@ export const MainContent = React.memo(
                 interactionGroup.interactions.forEach((interaction) => {
                   if (interactions.hasOwnProperty(interaction)) {
                     const resource = interactions[interaction] as JsonRpcInteractionType;
-                    const resourceKey = `${resource.path.slice(1).replace(/({|})/gi, '-')}-${
+                    const resourceKey = `${resource.path.replace(/({|})/gi, '-')}-${
                       resource.method
                     }`;
 
                     jdocList.push(
                       <JsonRpcResource interaction={resource} resourceKey={resourceKey} />
                     );
-
                     jdocPositions.push(resourceKey);
                   }
                 });
@@ -254,7 +253,7 @@ export const MainContent = React.memo(
         if (userEnums) {
           jdocList.push(<h2 className="reusable-header">Enums</h2>);
 
-          map(jdocExchange.userEnums, (userEnum, key) =>
+          map(jdocExchange.userEnums, (userEnum, key) => {
             jdocList.push(
               <ReusableResource
                 name={key}
@@ -265,8 +264,9 @@ export const MainContent = React.memo(
                 content={userEnum.value}
                 className="reusable-resource"
               />
-            )
-          );
+            );
+            jdocPositions.push(key);
+          });
         }
         setJdocList(jdocList);
         setJdocPositions(jdocPositions);
--- src/components/SidebarContent/SidebarLink.tsx
+++ src/components/SidebarContent/SidebarLink.tsx
@@ -9,20 +9,15 @@ interface SidebarLinkProps {
   resource: string;
 }
 
-const {isExport} = window as any;
-
 export const SidebarLink = ({linkTo, resource}: SidebarLinkProps) => {
   const {path} = useParams<MainRouterParams>();
   const {setCurrentUrl, currentUrl} = useContext(CurrentUrlContext);
 
   return (
-    <li
-      className={clsx([{active: linkTo.slice(1) === (isExport ? currentUrl : path)}])}
-      key={resource}
-    >
+    <li className={clsx([{active: linkTo === (currentUrl || path)}])} key={resource}>
       <span
         onClick={() => {
-          setCurrentUrl(linkTo.slice(1));
+          setCurrentUrl(linkTo);
         }}
       >
         {resource}
