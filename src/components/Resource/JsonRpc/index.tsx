c1bca76351e185a8fa187194ef9635512110140f
alvitsit <al.tselischev@gmail.com>
2022-10-06 09:50:57 +0400

    fixed display of rules on the right side

--- src/components/Resource/JsonRpc/index.tsx
+++ src/components/Resource/JsonRpc/index.tsx
@@ -11,30 +11,32 @@ interface JsonRpcResourceProps {
 
 export const JsonRpcResource = ({interaction, resourceKey}: JsonRpcResourceProps) => {
   return (
-    <div className="resource-wrapper">
-      <JsonRpcMethod
-        method={interaction.method}
-        annotation={interaction.annotation}
-        description={interaction.description}
-      />
-      {interaction.params && (
-        <ResourceBlock
-          keyBlock={`${resourceKey}_params`}
-          title="Params"
-          type={'code'}
-          data={interaction.params}
-          className="json-rpc-resource params"
+    <div className="resource-content">
+      <div className="resource-wrapper">
+        <JsonRpcMethod
+          method={interaction.method}
+          annotation={interaction.annotation}
+          description={interaction.description}
         />
-      )}
-      {interaction.result && (
-        <ResourceBlock
-          keyBlock={`${resourceKey}_result`}
-          title="Result"
-          type={'code'}
-          data={interaction.result}
-          className="json-rpc-resource result"
-        />
-      )}
+        {interaction.params && (
+          <ResourceBlock
+            keyBlock={`${resourceKey}_params`}
+            title="Params"
+            type={'code'}
+            data={interaction.params}
+            className="json-rpc-resource params"
+          />
+        )}
+        {interaction.result && (
+          <ResourceBlock
+            keyBlock={`${resourceKey}_result`}
+            title="Result"
+            type={'code'}
+            data={interaction.result}
+            className="json-rpc-resource result"
+          />
+        )}
+      </div>
     </div>
   );
 };
