b5bbc66dcdbc826aed9cf078163f7b096471ba47
kolnogorov <sq.skrju@gmail.com>
2023-02-16 17:12:17 +0300

    JDOC-777: Logo link changed, Message validator menu item added

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -4,8 +4,8 @@ import logo from 'assets/images/Logo.svg';
 
 export const HeaderLogo = () => (
   <div className="logo">
-    <Link to="/">
+    <a href="https://jsight.io" target="_blank" rel="noreferrer noopener">
       <img src={logo} alt="JSight" />
-    </Link>
+    </a>
   </div>
 );
