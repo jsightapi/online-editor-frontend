1e9e5f250ce256229f026af9ec78dc9990a3e407
Alexander Kolnogorov <sq.skrju@gmail.com>
2024-03-29 15:14:27 +0300

    JDOC-780 URL fix (#227)
    * JDOC-774 TableView: Path variables start from root object
    
    * JDOC-780 API update and htmldoc in htmlpreview fix
    
    * API_URL update in dockerfile
    
    * JDOC-780 Updates
    
    * JDOC-780 Url fixes

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,3 +1,3 @@
-export const baseUrl = process.env.REACT_APP_API_URL || `${window.location.origin}/api`;
+export const baseUrl = process.env.REACT_APP_API_URL || `${window.location.origin}`;
 
 export const convertJsightUrl = `${baseUrl}/convert-jsight`;
